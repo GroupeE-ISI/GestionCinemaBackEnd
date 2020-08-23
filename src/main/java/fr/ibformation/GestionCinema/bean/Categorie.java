3512e2b1792c0295bab99a5525c34cb4f886c55b
Ilham_Benhsine <benhsine.ilham@gmail.com>
2020-08-23 11:00:06 +0200

    Merge

--- src/main/java/fr/ibformation/GestionCinema/bean/Categorie.java
+++ src/main/java/fr/ibformation/GestionCinema/bean/Categorie.java
@@ -1,17 +1,10 @@
 package fr.ibformation.GestionCinema.bean;
 
-
-
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
